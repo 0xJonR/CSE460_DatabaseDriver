b74326fe39af575409910b7c6a3105cc9b36a9d2
jon <jr367@buffalo.edu>
2021-10-17 19:38:14 -0400

    Updated DatabaseDriver.go. Now completely replaces the shell script and cna download all database files on any platform that has Golang installed. Also has descriptive text for users, and uses goroutines to download all big files concurrently. Added TODOs as well.

--- DatabaseDriver.go
+++ DatabaseDriver.go
@@ -1,9 +1,25 @@
+/*
+CSE460 Database Driver Script
+execute with 'go run .'
+Written by Jon Rodriguez
+October 2021
+-------------------------------
+TODO:
+1) Add Unzip functionality to .gz files after downloading
+2) Add SQL Driver code to create database and UPDATE with new data
+3) Add ability to create minified version of database
+*/
 package main
 
 //imports
 import(
 	"fmt"
 
+	"net/http"
+
+	"os"
+
+	"io"
 )
 
 //constants: 
@@ -24,7 +40,53 @@ func buildLink(which string) string {
 	l := IMDB_SRC_LINK + which
 	return l
 }
+
+/* grab makes an http GET request to the link
+and returns the byte representation of the data.
+*/
+func grab(fp string, link string, finished chan bool) (error) {
+	// create file handle
+	out, err := os.Create(fp)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	//GET data
+	fmt.Println("Downloading: ", fp)
+	resp, err2 := http.Get(link)
+	if err2 != nil {
+		fmt.Println("bad GET")
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+	//write the body to file
+	_, err3 := io.Copy(out, resp.Body)
+	if err3 != nil {
+		return err3
+	}
+	defer func(){
+		fmt.Printf("\nFinished downloading to file: %s", fp)
+	}()
+	finished <- true
+	return nil
+}
 func main() {
-	fmt.Println("test")
-	fmt.Println(buildLink(IMDB_title_akas))
+	imdb_fns := [7]string{IMDB_name_basics, IMDB_title_akas, IMDB_title_basics, IMDB_title_crew, IMDB_title_episode, IMDB_title_principals, IMDB_title_ratings}
+	finished := make(chan bool)
+	//fmt.Println(imdb_fns)
+	for i := 0; i<7; i++ {
+		fn := imdb_fns[i]
+		link := buildLink(fn)
+		fmt.Println(fn)
+		go grab(fn, link, finished) //goroutine for speed :D concurrency ftw 
+	}
+	for g :=0; g<7; g++ {
+		<-finished
+	}
+	fmt.Println("\nFinsihed.")
 }
